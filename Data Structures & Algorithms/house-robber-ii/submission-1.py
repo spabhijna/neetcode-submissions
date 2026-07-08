8534ceeed047d180552cc911030602beb5eb4814
Abhijna SP <146448251+spabhijna@users.noreply.github.com>
2026-07-08 23:09:04 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) <= 2:
+            return max(nums)
+        
+        def linear_rob(start: int, end: int) -> int:
+            prev2 = nums[start]
+            prev1 = max(nums[start], nums[start + 1])
+            
+            for i in range(start + 2, end + 1):
+                curr = max(prev1, nums[i] + prev2)
+                prev2 = prev1
+                prev1 = curr
+            return prev1
+
+        return max(linear_rob(0, len(nums) - 2), linear_rob(1, len(nums) - 1))
